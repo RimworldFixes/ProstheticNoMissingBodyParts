b754e3dc16d31152fd680cc70655fe84387d58c3
Stepan Kamentsev <skamentsev@naumen.ru>
2020-05-21 03:38:09 +0300

    Mod source code, version 1.0

---
+++ Source/ProstheticNoMissingBodyParts/HarmonyPatches.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace ProstheticNoMissingBodyParts
+{
+    
+    [StaticConstructorOnStartup]
+    partial class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony("com.prostheticnomissingbodyparts.rimworld.mod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Log.Message(
+                "ProstheticNoMissingBodyParts Harmony Patches:" + Environment.NewLine +
+                "  Prefix:" + Environment.NewLine +
+                "    ApparelUtility.HasPartsToWear");
+        }
+    }
+    
+    [Harmony]
+    
+    [HarmonyPatch(typeof(ApparelUtility), "HasPartsToWear")]
+    static class Patch_ApparelUtility_HasPartsToWear
+    {
+        static bool Prefix(ref bool __result, Pawn p, ThingDef apparel)
+        {
+            // *****
+            // Original Rimworld code start
+            // ****
+            
+            List<Hediff> hediffs = p.health.hediffSet.hediffs;
+            bool flag = false;
+            for (int index = 0; index < hediffs.Count; ++index)
+            {
+                if (hediffs[index] is Hediff_MissingPart)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+            {
+                __result = true;
+                return false;
+            }
+            IEnumerable<BodyPartRecord> notMissingParts = p.health.hediffSet.GetNotMissingParts(BodyPartHeight.Undefined, BodyPartDepth.Undefined, (BodyPartTagDef) null, (BodyPartRecord) null);
+            List<BodyPartGroupDef> groups = apparel.apparel.bodyPartGroups;
+            for (int i = 0; i < groups.Count; i++)
+            {
+                if (notMissingParts.Any<BodyPartRecord>((Func<BodyPartRecord, bool>) (x => x.IsInGroup(groups[i]))))
+                {
+                    __result = true;
+                    return false;
+                }
+            }
+            
+            // *****
+            // Original Rimworld code end
+            // ****
+
+            
+            // *****
+            // ProstheticNoMissingBodyParts patch code start
+            // *****
+            
+            // this code run only if original Rimworld check return false (body part is missed)
+            // and we try to fix this, if pawn has a bionic part in right place (Bionic Arm, Bionic Leg, etc...)
+            
+            // load mod settings with whitelisted bionic parts for arms and legs
+            var settings = LoadedModManager
+                .GetMod<ProstheticNoMissingBodyPartsMod>()
+                .GetSettings<ProstheticNoMissingBodyPartsSettings>();
+
+            // prepare hash sets for quicker checks
+            var armsWhitelist = new HashSet<string>();
+            var legsWhitelist = new HashSet<string>();
+
+            // for arms
+            if (settings.ArmsWhitelist != null)
+            {
+                armsWhitelist.AddRange(settings.ArmsWhitelist);
+            }
+            
+            // for legs
+            if (settings.LegsWhitelist != null)
+            {
+                legsWhitelist.AddRange(settings.LegsWhitelist);
+            }
+            
+            // prepare bool set for apparel body part groups
+            var isLeftHand = false;
+            var isRightHand = false;
+            var isHands = false;
+            var isFeet = false;
+
+            foreach (var g in groups)
+            {
+                switch (g.defName)
+                {
+                    case "LeftHand":
+                        // really this for fingers :) 
+                        isLeftHand = true;
+                        break;
+                    case "RightHand":
+                        // and this too
+                        isRightHand = true;
+                        break;
+                    case "Hands":
+                        isHands = true;
+                        break;
+                    case "Feet":
+                        isFeet = true;
+                        break;
+                }
+            }
+            
+            // check if apparel needed left hand, especially for jewelry mod, pawns try to wear bracer... on fingers... omg...
+            if (isLeftHand)
+            {
+                __result = hediffs.Exists((h) =>
+                {
+                    if (h.Part?.customLabel != null && h.def?.defName != null)
+                    {
+                        // true if left arm replaced with whitelisted bionic part
+                        // here we can catch an issue if label were translated
+                        return h.Part.customLabel.ToLower().Equals("left shoulder") &&
+                               armsWhitelist.Contains(h.def.defName);
+                    }
+
+                    return false;
+                });
+                return false;
+            }
+            
+            // check if apparel needed right hand, especially for jewelry, wearing rings?
+            if (isRightHand)
+            {
+                __result = hediffs.Exists((h) =>
+                {
+                    if (h.Part?.customLabel != null && h.def?.defName != null)
+                    {
+                        // true if any right arm replaced with whitelisted bionic part
+                        // here we can catch an issue if label were translated                        
+                        return h.Part.customLabel.ToLower().Equals("right shoulder") &&
+                               armsWhitelist.Contains(h.def.defName);
+                    }
+
+                    return false;
+                });
+                return false;
+            }
+            
+            // check if apparel needed hands, useful for gloves
+            if (isHands)
+            {
+                __result = hediffs.Exists((h) =>
+                {
+                    if (h.Part?.def?.defName != null && h.def?.defName != null)
+                    {
+                        // true if any arm replaced with whitelisted bionic part
+                        return h.Part.def.defName.Equals("Shoulder") &&
+                               armsWhitelist.Contains(h.def.defName);
+                    }
+
+                    return false;
+                });
+                return false;
+            }
+            
+            // check if apparel needed feet, useful for boots
+            if (isFeet)
+            {
+                __result = hediffs.Exists((h) =>
+                {
+                    if (h.Part?.def?.defName != null && h.def?.defName != null)
+                    {
+                        // true if any leg replaced with whitelisted bionic part
+                        return h.Part.def.defName.Equals("Leg") &&
+                               legsWhitelist.Contains(h.def.defName);
+                    }
+
+                    return false;
+                });
+                return false;
+            }            
+            
+            // *****
+            // ProstheticNoMissingBodyParts patch code end
+            // *****
+            
+            // body part still missed
+            
+            __result = false;
+            return false;
+        }
+    }
+}
---
+++ Source/ProstheticNoMissingBodyParts/ModSettings.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections.Generic;
+using Verse;
+using UnityEngine;
+
+namespace ProstheticNoMissingBodyParts
+{
+    public class ProstheticNoMissingBodyPartsSettings : ModSettings
+    {
+        // default whitelisted arms, Vanilla and EPOE
+        public List<string> ArmsWhitelist = new List<string>()
+        {
+            "SimpleProstheticArm", 
+            "BionicArm", 
+            "AdvancedBionicArm",
+            "ArchotechArm"
+        };
+        
+        // default whitelisted legs, Vanilla and EPOE
+        public List<string> LegsWhitelist = new List<string>()
+        {
+            "SimpleProstheticLeg", 
+            "BionicLeg", 
+            "AdvancedBionicLeg",
+            "ArchotechLeg",
+            "MuscleStimulatorLegs"
+        };
+        
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref ArmsWhitelist, "armsWhitelist", LookMode.Value);
+            Scribe_Collections.Look(ref LegsWhitelist, "legsWhitelist", LookMode.Value);
+        }
+    }
+    
+    public class ProstheticNoMissingBodyPartsMod : Mod
+    {
+        private readonly ProstheticNoMissingBodyPartsSettings _settings;
+        private static ProstheticNoMissingBodyPartsMod _mod;
+        
+        private readonly Dictionary<string, bool[]> _armsWhitelistMap = new Dictionary<string, bool[]>();
+        private readonly Dictionary<string, bool[]> _legsWhitelistMap = new Dictionary<string, bool[]>();
+
+        private readonly List<HediffDef> _armsHediff = new List<HediffDef>();
+        private readonly List<HediffDef> _legsHediff = new List<HediffDef>();
+        
+        private string _armSearchQuery = "";
+        private string _legSearchQuery = "";
+
+        private Vector2 _armsViewScroll = new Vector2(0,0);
+        private Vector2 _legsViewScroll = new Vector2(0,0);
+
+        private bool _isInitialized;
+        
+        public ProstheticNoMissingBodyPartsMod(ModContentPack content) : base(content)
+        {
+            _settings = GetSettings<ProstheticNoMissingBodyPartsSettings>();
+            _mod = this;
+        }
+
+        public override void WriteSettings()
+        {
+            Log.Message("[ProstheticNoMissingBodyParts] Save settings");
+
+            if (_mod._settings.ArmsWhitelist != null)
+            {
+                _mod._settings.ArmsWhitelist.Clear();
+                foreach (var kv in _armsWhitelistMap)
+                {
+                    if (kv.Value[0])
+                    {
+                        _mod._settings.ArmsWhitelist.Add(kv.Key);
+                    }
+                }
+            }
+            
+            if (_mod._settings.LegsWhitelist != null)
+            {
+                _mod._settings.LegsWhitelist.Clear();
+                foreach (var kv in _legsWhitelistMap)
+                {
+                    if (kv.Value[0])
+                    {
+                        _mod._settings.LegsWhitelist.Add(kv.Key);
+                    }
+                }
+            }
+
+            base.WriteSettings();
+        }
+
+        private void Init()
+        {
+            //check if init already done
+            if (_isInitialized) return;
+            
+            _isInitialized = true;
+            
+            Log.Message("[ProstheticNoMissingBodyParts] Init Settings");
+            
+            if (_mod._settings.ArmsWhitelist == null)
+            {
+                _mod._settings.ArmsWhitelist = new List<string>();
+            }
+
+            if (_mod._settings.LegsWhitelist == null)
+            {
+                _mod._settings.LegsWhitelist = new List<string>();
+            }            
+            
+            // sets for checkboxes default checks
+            var currentArmsSet = new HashSet<string>(_mod._settings.ArmsWhitelist);
+            var currentLegsSet = new HashSet<string>(_mod._settings.LegsWhitelist);
+
+            // load all recipes definitions that replace original (natural) arms or legs and extract hediff from it
+            foreach (var recipeDef in DefDatabase<RecipeDef>.AllDefs)
+            {
+                if (recipeDef.appliedOnFixedBodyParts == null || recipeDef.addsHediff == null) continue;
+                
+                // catch arm
+                if (recipeDef.appliedOnFixedBodyParts.Exists((x) => x.defName.Equals("Shoulder")))
+                {
+                    Log.Message("[ProstheticNoMissingBodyParts] Add Shoulder " + recipeDef.addsHediff.defName);
+                    _armsWhitelistMap[recipeDef.addsHediff.defName] = new[] { currentArmsSet.Contains(recipeDef.addsHediff.defName) };
+                    _armsHediff.Add(recipeDef.addsHediff);
+                }
+                        
+                // catch leg
+                if (recipeDef.appliedOnFixedBodyParts.Exists((x) => x.defName.Equals("Leg")))
+                {
+                    Log.Message("[ProstheticNoMissingBodyParts] Add Leg " + recipeDef.addsHediff.defName);
+                    _legsWhitelistMap[recipeDef.addsHediff.defName] = new[] { currentLegsSet.Contains(recipeDef.addsHediff.defName) };
+                    _legsHediff.Add(recipeDef.addsHediff);
+                }
+            }
+        }
+        
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Init();
+            
+            var listingStandard = new Listing_Standard();
+            
+            
+            // arms settings GUI
+            var armsGroup = new Rect(inRect.x, inRect.y, inRect.width, 180f);
+            var armsSearchRect = new Rect(inRect.width - 200f, armsGroup.y, 200f, 24f);
+            
+            _armSearchQuery = Widgets.TextArea(armsSearchRect, _armSearchQuery);
+            var armSearchQueryIsEmpty = _armSearchQuery.NullOrEmpty();
+            
+            listingStandard.Begin(armsGroup);
+            listingStandard.Label("ProstheticNoMissingBodyPartsWhitelistedArmsName".Translate());
+            listingStandard.End();
+
+            var filteredArms = _armsHediff.FindAll((x) => 
+                armSearchQueryIsEmpty || 
+                x.defName.ToLower().Contains(_armSearchQuery.ToLower()) || 
+                x.label.ToLower().Contains(_armSearchQuery.ToLower())
+            );
+            
+            var armsHolder = new Rect(armsGroup.x, armsGroup.y + 30, armsGroup.width, 150f);
+            var armsView = new Rect(armsHolder.x, armsHolder.y, armsGroup.width - 24f, filteredArms.Count * 24f);
+
+            Widgets.BeginScrollView(armsHolder, ref _armsViewScroll, armsView);
+            listingStandard.Begin(armsView);
+
+            foreach (var hediffDef in filteredArms)
+            {
+                listingStandard.CheckboxLabeled(
+                    hediffDef.label.CapitalizeFirst() + " (" + hediffDef.defName + ")",
+                    ref _armsWhitelistMap[hediffDef.defName][0],
+                    hediffDef.description
+                );
+            }
+            
+            listingStandard.End();
+            Widgets.EndScrollView();
+            
+            
+            // legs setting GUI
+            var legsGroup = new Rect(inRect.x, inRect.y + 230f, inRect.width, 180f);
+            var legsSearchRect = new Rect(inRect.width - 200f, legsGroup.y, 200f, 24f);
+            
+            _legSearchQuery = Widgets.TextArea(legsSearchRect, _legSearchQuery);
+            var legSearchQueryIsEmpty = _legSearchQuery.NullOrEmpty();
+            
+            listingStandard.Begin(legsGroup);
+            listingStandard.Label("ProstheticNoMissingBodyPartsWhitelistedLegsName".Translate());
+            listingStandard.End();
+            
+            var filteredLegs = _legsHediff.FindAll((x) => 
+                legSearchQueryIsEmpty || 
+                x.defName.ToLower().Contains(_legSearchQuery.ToLower()) ||
+                x.label.ToLower().Contains(_legSearchQuery.ToLower())
+            );            
+            
+            var legsHolder = new Rect(legsGroup.x, legsGroup.y + 30, legsGroup.width, 150f);
+            var legsView = new Rect(legsHolder.x, legsHolder.y, legsGroup.width - 24f, filteredLegs.Count * 24f);
+            
+            Widgets.BeginScrollView(legsHolder, ref _legsViewScroll, legsView);
+            listingStandard.Begin(legsView);
+            
+            foreach (var hediffDef in filteredLegs)
+            {
+                listingStandard.CheckboxLabeled(
+                    hediffDef.label.CapitalizeFirst() + " (" + hediffDef.defName + ")",
+                    ref _legsWhitelistMap[hediffDef.defName][0],
+                    hediffDef.description
+                );
+            }            
+            
+            listingStandard.End();
+            Widgets.EndScrollView();
+            
+            base.DoSettingsWindowContents(inRect);
+        }
+        
+        public override string SettingsCategory()
+        {
+            return "ProstheticNoMissingBodyPartsModName".Translate();
+        }
+    }
+}
